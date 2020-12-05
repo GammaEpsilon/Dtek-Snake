b59c4dc7a92a7215fc38511d18887a868d031fbd
unknown <vinsa@kth.se>
2020-12-05 19:09:06 +0100

    Edited version for assemblermaker

--- src/snake.c
+++ src/snake.c
@@ -4,7 +4,7 @@
 #define SNAKE_BUFFER 255
 #define APPLE_REPR 127
 #define EPSTEIN_KILLED_HIMSELF 0
-#define reset_grid(grid, dimensions) for (int i = dimensions.x*dimensions.y; i; grid[i--] = 0)
+#define reset_grid(grid, dimensions) { int i = dimensions.x*dimensions.y; while(i) grid[i--] = 0; }
 #define generate_apple(grid, dimensions) { do { appleLoc = rand()%(dimensions.x*dimensions.y); } while (grid[appleLoc]); grid[appleLoc] = APPLE_REPR; }
 
 #include "snake.h"
@@ -61,13 +61,16 @@ void init(snake *snake, cord dimensions) {
     snake->head = 0;
     snake->id = createdSnakes++;
     snake->movement = RIGHT;
-    for (int i = 1; i--; snake->grow=1) move(snake); //Set initial size to 3
+    int i;
+    for (i = 1; i--; snake->grow=1) move(snake); //Set initial size to 3
 }
 
 // Returns != 0 when game over
 char updateGrid(unsigned char *grid, snake *snakes, unsigned char noOfSnakes, cord dimensions) {
     char bitflag = 0;
-    for (int temp, i = 0; i < noOfSnakes; i++) {
+    int temp;
+    int i;
+    for (temp, i = 0; i < noOfSnakes; i++) {
         cord snakeCords = snakes[i].buffer[snakes[i].head];
         if (snakeCords.x > dimensions.x || snakeCords.x < 0 //Check for hitting wall on x-axis
         || (snakeCords.y > dimensions.y || snakeCords.y < 0) //Check for hitting wall on y-axis
@@ -83,7 +86,8 @@ char updateGrid(unsigned char *grid, snake *snakes, unsigned char noOfSnakes, co
             if (!i) //Only do this the first iteraiton
                 reset_grid(grid, dimensions); //Reset grid
             grid[snakeCords.x + snakeCords.y*dimensions.x] = snakes[i].id; //Update snakeposition
-            for (int j = snakes[i].tail; j != snakes[i].head; ++j%SNAKE_BUFFER)
+            int j;
+            for (j = snakes[i].tail; j != snakes[i].head; ++j%SNAKE_BUFFER)
                 grid[snakes[i].buffer[j].x + snakes[i].buffer[j].y*dimensions.x] = snakes[i].id;
         }
     }
@@ -98,23 +102,25 @@ void game_init(char noOfSnakes, int x, int y, unsigned char *grid) {
     dims.y = y;
     noOfS = noOfSnakes;
     reset_grid(grid, dims);
-    for (int i = 0; i < noOfS; i++) init(snakes + i, dims);
+    int i;
+    for (i = 0; i < noOfS; i++) init(snakes + i, dims);
     generate_apple(grid, dims);
 }
 
 char turn(enum movement *movement, unsigned char * output) {
     int bitflag;
-    for (int i = 0; i < noOfS; i++) {
-        if (movement[i])
-            snakes[i].movement = movement[i];
-        move(snakes + i);
+    int j;
+    for (j = 0; j < noOfS; j++) {
+        if (movement[j])
+            snakes[j].movement = movement[j];
+        move(snakes + j);
     }
     if (!(bitflag = updateGrid(output, snakes, noOfS, dims))) { //Not gameover
         return 0;
     } else {
-        int i;
+        char i;
         int index = 0;
-        for (char i = 0; i < 8; i++) {
+        for (i = 0; i < 8; i++) {
             if ((1<<i)&bitflag)
                 index += sprintf(output + index, "Game over, player %d lost\n", i);
         }//Clean up
